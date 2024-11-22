5c69c74af54f1a7a2eb8417c38fcd6228ecf8f35
Ismailjabiullah82 <ijabiullah@gmail.comm>
2024-11-22 09:29:10 +0400

    added tailwind.config.js & others

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl font-bold'>React Price Options</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
